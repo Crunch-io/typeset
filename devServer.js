249bccd81f78e3735ae40cb24a8bb0639380e62f
April Arcus <april.arcus@gmail.com>
2015-10-26 17:34:19 -0700

    webpack

--- devServer.js
+++ devServer.js
@@ -1,9 +1,19 @@
 import path from 'path';
 import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import config from './webpack.config';
+
+const compiler = webpack(config);
 
 const app = express();
 
-app.use(express.static('.'));
+app.use(express.static('./examples'));
+
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.listen(3000, 'localhost', err => {
   if (err) {
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  devtool: 'cheap-module-source-map',
+  entry: {
+    article: './examples/article/index.js',
+    flatland: './examples/flatland/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
+};
